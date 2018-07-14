240415144719257bbfe128d0d0ffe3dc98a504aa
Olivier St-Georges <olivierstgeorges1@gmail.com>
2018-07-14 10:23:17 -0400

    structure code

--- python/ai.py
+++ python/ai.py
@@ -24,16 +24,20 @@ class AI:
         resources = [(1, 0.1, v) for v in game.resources.regular] + [(1, 2, v) for v in game.resources.silver] + [(10, 0, v) for v in game.resources.gold]
 
         for cell in game.me.cells:
+            target = get_target(cell, resources)
+            
 
-            resources_score_distance_ratio = []
-
-            for resource in resources:
-                print(resource[2])
-                distance = cell.position.distance_to(resource[2])
-                ratio = resource[0] / distance
-                resources_score_distance_ratio.append(ratio)
-
-            target_resource_index = resources_score_distance_ratio.index(max(resources_score_distance_ratio))
-
-            print(resources[target_resource_index][2])
             cell.move(resources[target_resource_index][2])
+
+    def get_target(self, cell, resources):
+        resources_score_distance_ratio = []
+
+        for resource in resources:
+            print(resource[2])
+            distance = cell.position.distance_to(resource[2])
+            ratio = resource[0] / distance
+            resources_score_distance_ratio.append(ratio)
+
+        target_resource_index = resources_score_distance_ratio.index(max(resources_score_distance_ratio))
+
+        return resources[target_resource_index][2]
