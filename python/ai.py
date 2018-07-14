79c8f0db6d5a73195cd83a14366a5e4d2d6746a9
Olivier St-Georges <olivierstgeorges1@gmail.com>
2018-07-14 10:18:42 -0400

    implement first ai

--- python/ai.py
+++ python/ai.py
@@ -21,10 +21,19 @@ class AI:
 
         print("Tick #{}".format(game.tick))
 
+        resources = [(1, 0.1, v) for v in game.resources.regular] + [(1, 2, v) for v in game.resources.silver] + [(10, 0, v) for v in game.resources.gold]
+
         for cell in game.me.cells:
-            distance = cell.position.distance_to(cell.target)
 
-            if distance < 10:
-                target = Vec2(random.randint(0, game.map.width), random.randint(0, game.map.height))
+            resources_score_distance_ratio = []
 
-                cell.move(target)
+            for resource in resources:
+                print(resource[2])
+                distance = cell.position.distance_to(resource[2])
+                ratio = resource[0] / distance
+                resources_score_distance_ratio.append(ratio)
+
+            target_resource_index = resources_score_distance_ratio.index(max(resources_score_distance_ratio))
+
+            print(resources[target_resource_index][2])
+            cell.move(resources[target_resource_index][2])
